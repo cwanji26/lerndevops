a9e7dd79166fd81c011292bacc9469b9e58ca0dd
cwanji26 <70167313+cwanji26@users.noreply.github.com>
2021-11-04 19:47:47 -0500

    Update setup-Jenkins.sh

--- labs/jenkins/setup-Jenkins.sh
+++ labs/jenkins/setup-Jenkins.sh
@@ -26,7 +26,7 @@ sudo systemctl start jenkins
 ## cd /tmp ; wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
 ## cd /tmp ; wget http://apachemirror.wuchna.com/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz
 cd /tmp ; wget http://apachemirror.wuchna.com/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-cd /tmp ; tar -xzf apache-maven-3.6.3-bin.tar.gz -C /opt
+cd /tmp ; tar -xzf apache-maven-3.8.3-bin.tar.gz -C /opt
 
 #Install SonarQube-runner ( on Jenkins machine )
 
